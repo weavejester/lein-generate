d01373265e19e6df6f13bfefd1f3da0c78bad268
James Reeves <jreeves@weavejester.com>
2014-04-02 00:53:33 +0100

    Add leiningen.generate.templates namespace

--- generators/src/leiningen/generate/namespace.clj
+++ generators/src/leiningen/generate/namespace.clj
@@ -1,6 +1,7 @@
 (ns leiningen.generate.namespace
   (:refer-clojure :exclude [namespace])
-  (:require [leiningen.new.templates :refer [renderer name-to-path ->files]]
+  (:require [leiningen.generate.templates :refer [renderer ->files]]
+            [leiningen.new.templates :refer [render-text name-to-path]]
             [leiningen.core.main :as main]))
 
 (def render (renderer "namespace"))
@@ -8,6 +9,6 @@
 (defn namespace
   "Generate a new namespace."
   [name]
-  (let [data {:namespace name, :sanitized (name-to-path name)}]
-    (main/info "Generating src/{{sanitized}}.clj")
+  (let [data {:name name, :sanitized (name-to-path name)}]
+    (main/info (render-text "Generating src/{{sanitized}}.clj" data))
     (->files data ["src/{{sanitized}}.clj" (render "source.clj" data)])))
---
+++ src/leiningen/generate/templates.clj
@@ -0,0 +1,39 @@
+(ns leiningen.generate.templates
+  "Functions for handling generator templates that are not in the
+  leiningen.new.templates namespace."
+  (:require [leiningen.new.templates :refer [sanitize render-text slurp-resource]]
+            [clojure.java.io :as io]
+            [clojure.string :as string]
+            [leiningen.core.main :as main]))
+
+(defn renderer
+  "Create a renderer function that looks for mustache templates in the
+  right place given the name of your template. If no data is passed, the
+  file is simply slurped and the content returned unchanged.
+
+  Functionally the same as leiningen.new.templates/renderer, except
+  templates are looked for in leiningen/generate rather than leiningen/new."
+  [name]
+  (fn [template & [data]]
+    (let [path (string/join "/" ["leiningen" "generate" (sanitize name) template])]
+      (if-let [resource (io/resource path)]
+        (if data
+          (render-text (slurp-resource resource) data)
+          (io/reader resource))
+        (main/abort (format "Template resource '%s' not found." path))))))
+
+(defn ->files
+  "Generate a file with content. path can be a java.io.File or string.
+  It will be turned into a File regardless. Any parent directories will
+  be created automatically."
+  [data & paths]
+  (doseq [path paths]
+    (if (string? path)
+      (.mkdirs (io/file (render-text path data)))
+      (let [[path content & options] path
+            path (io/file (render-text path data))
+            options (apply hash-map options)]
+        (.mkdirs (.getParentFile path))
+        (io/copy content (io/file path))
+        (when (:executable options)
+          (.setExecutable path true))))))
