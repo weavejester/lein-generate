7c64f257972bdd821ff2ec3a501c0571fa94b6e7
James Reeves <jreeves@weavejester.com>
2014-04-01 23:10:57 +0100

    Basic generator functionality

--- src/leiningen/generate.clj
+++ src/leiningen/generate.clj
@@ -1,6 +1,16 @@
-(ns leiningen.generate)
+(ns leiningen.generate
+  (:require [leiningen.core.project :as project]
+            [leiningen.core.main :refer [abort]]))
+
+(defn resolve-generator [name]
+  (let [sym (symbol (str "leiningen.generate." name))]
+    (if (try (require sym) true
+             (catch java.io.FileNotFoundException _ false))
+      (resolve (symbol (str sym "/" name)))
+      (abort "Could not find template" name "on the classpath."))))
 
 (defn generate
-  "I don't do a lot."
-  [project & args]
-  (println "Hi!"))
+  "Generate files from a template."
+  [project template & args]
+  (project/load-plugins project :generators)
+  (apply (resolve-generator template) args))
