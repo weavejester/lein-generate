4f1c9e34fa5bfcb997ecb7126f50f33334ec7e50
James Reeves <jreeves@weavejester.com>
2014-04-10 23:27:04 +0100

    Add :scm key to generators and template subprojects

--- generators/project.clj
+++ generators/project.clj
@@ -1,6 +1,7 @@
 (defproject lein-gen/generators "0.1.0-SNAPSHOT"
   :description "A basic set of generators for lein-gen"
   :url "https://github.com/weavejester/lein-gen"
+  :scm {:dir ".."}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
--- template/project.clj
+++ template/project.clj
@@ -1,6 +1,7 @@
 (defproject generators/lein-template "0.1.0-SNAPSHOT"
   :description "Basic generators for the lein-gen plugin"
   :url "https://github.com/weavejester/lein-gen"
+  :scm {:dir ".."}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
