8cd3e1746affad54b6f2021111202939cf8a195f
James Reeves <jreeves@weavejester.com>
2014-10-27 00:46:35 +0000

    Add :scm key to plugin project file

--- plugin/project.clj
+++ plugin/project.clj
@@ -1,6 +1,7 @@
 (defproject lein-gen "0.1.1"
   :description "Lein plugin for generating files"
   :url "https://github.com/weavejester/lein-gen"
+  :scm {:dir ".."}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
