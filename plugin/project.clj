4739f88d031b292eec6920ce08ca626c7e631d29
James Reeves <jreeves@weavejester.com>
2014-10-27 00:45:48 +0000

    Remove generators dev dependency from plugin

--- plugin/project.clj
+++ plugin/project.clj
@@ -3,6 +3,4 @@
   :url "https://github.com/weavejester/lein-gen"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :eval-in-leiningen true
-  :profiles
-  {:dev {:generators [[lein-gen/generators "0.1.1"]]}})
+  :eval-in-leiningen true)
