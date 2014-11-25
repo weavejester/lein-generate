a9fd98fb452e2ca81fcee27bba7fbed29bf451d9
James Reeves <jreeves@weavejester.com>
2014-11-25 22:29:17 +0000

    Use create-file function in namespace generator

--- generators/src/leiningen/generate/namespace.clj
+++ generators/src/leiningen/generate/namespace.clj
@@ -1,17 +1,13 @@
 (ns leiningen.generate.namespace
   (:refer-clojure :exclude [namespace])
-  (:require [leiningen.generate.templates :refer [renderer ->files]]
-            [leiningen.new.templates :refer [render-text name-to-path]]
+  (:require [leiningen.generate.templates :as tmpl]
+            [leiningen.new.templates :refer [name-to-path]]
             [leiningen.core.main :as main]))
-
-(def render (renderer "namespace"))
 
 (defn namespace
   "Generate a new namespace and test file."
   [project name]
   (let [data {:name name, :sanitized (name-to-path name)}]
-    (main/info (render-text "Generating src/{{sanitized}}.clj" data))
-    (->files data ["src/{{sanitized}}.clj" (render "source.clj" data)])
-
-    (main/info (render-text "Generating test/{{sanitized}}_test.clj" data))
-    (->files data ["test/{{sanitized}}_test.clj" (render "test.clj" data)])))
+    (doto (tmpl/renderer "namespace")
+      (tmpl/create-file "source.clj" "src/{{sanitized}}.clj" data)
+      (tmpl/create-file "test.clj" "test/{{sanitized}}_test.clj" data))))
